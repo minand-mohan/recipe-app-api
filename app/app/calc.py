24b8223bfbec5b2aa540f56cc6afa45656e657a2
Minand Manomohanan <minand@esper.io>
2023-03-22 14:07:02 +0530

    Add dummy tests

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+Calculator functioncs
+"""
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_number(self):
+        res = calc.add(5, 6)
+
+        self.assertEquals(res, 11)
+
+    def test_subtract_number(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEquals(res, 5)
