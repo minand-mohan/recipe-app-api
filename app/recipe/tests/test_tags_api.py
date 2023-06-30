6270f01e9b985a65f9ea4ffd7c5c8bacecb69f04
Minand Manomohanan <minand@esper.io>
2023-06-30 20:29:27 +0530

    Add filter for tags and ingredients

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,6 +1,8 @@
 """
 Tests for tags API.
 """
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -8,7 +10,10 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import (
+    Tag,
+    Recipe,
+)
 
 from recipe.serializers import TagSerializer
 
@@ -93,3 +98,45 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         tags = Tag.objects.filter(user=self.user)
         self.assertFalse(tags.exists())
+
+    def test_filter_tags_assigned_recipes(self):
+        """Test listing tags to those assigned to recipes."""
+        tag1 = Tag.objects.create(user=self.user, name='Indian')
+        tag2 = Tag.objects.create(user=self.user, name='Italian')
+        recipe1 = Recipe.objects.create(
+            title='Paneer Butter Masala',
+            time_minutes=70,
+            price=Decimal('8.00'),
+            user=self.user
+        )
+        recipe1.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        s1 = TagSerializer(tag1)
+        s2 = TagSerializer(tag2)
+        self.assertIn(s1.data, res.data)
+        self.assertNotIn(s2.data, res.data)
+
+    def test_filtered_tags_unique(self):
+        """Test if filtered tags are unique"""
+        tag1 = Tag.objects.create(user=self.user, name='Indian')
+        Tag.objects.create(user=self.user, name='Italian')
+        recipe1 = Recipe.objects.create(
+            title='Paneer Butter Masala',
+            time_minutes=70,
+            price=Decimal('8.00'),
+            user=self.user
+        )
+        recipe2 = Recipe.objects.create(
+            title='Chicken Butter Masala',
+            time_minutes=70,
+            price=Decimal('8.00'),
+            user=self.user
+        )
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
